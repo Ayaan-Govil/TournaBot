5db33413e611dcbd34c0f1e43e5e478cc8e9069f
Ayaan-Govil <38332695+Ayaan-Govil@users.noreply.github.com>
2020-12-28 18:53:17 -0800

    Removed unnecessary catches

--- commands/search.js
+++ commands/search.js
@@ -11,30 +11,31 @@ module.exports = {
     // IDs:
     // Smash Ultimate: 1386
     // Valorant: 34223
-    if (message.channel instanceof Discord.DMChannel) { sendMessage(message, 'I cannot run this command in DMs.') }//do not execute
-    else //execute
-      if (message.member.hasPermission('ADMINISTRATOR')) {
-        let gameArgs = message.content.split(' ');
-        gameArgs.shift();
-        let game;
-        let gameFound = false;
-        let gameName;
-        if (gameArgs.length >= 1) {
-          if (gameArgs.join(' ').toLowerCase() == 'super smash bros. ultimate') {
-            game = 1386;
-            gameFound = true;
-            gameName = 'Super Smash Bros. Ultimate';
-          } else if (gameArgs.join(' ').toLowerCase() == 'valorant') {
-            game = 34223;
-            gameFound = true;
-            gameName = 'Valorant'
-          } else { sendMessage(message, `I could not find the specified game. Do \`t!help\` to get command info.`); }
+    // if (!(message.channel instanceof Discord.DMChannel) && !message.member.hasPermission('ADMINISTRATOR')) {
+    //   message.channel instanceof Discord.DMChannel ? sendMessage(message, 'You don\'t have the permissions for that :sob:', 'REPLY') : sendMessage(message, 'you don\'t have the permissions for that :sob:', 'REPLY');
+    //   return;
+    // }
+    let gameArgs = message.content.split(' ');
+    gameArgs.shift();
+    let game;
+    let gameFound = false;
+    let gameName;
+    if (gameArgs.length >= 1) {
+      if (gameArgs.join(' ').toLowerCase() == 'super smash bros. ultimate') {
+        game = 1386;
+        gameFound = true;
+        gameName = 'Super Smash Bros. Ultimate';
+      } else if (gameArgs.join(' ').toLowerCase() == 'valorant') {
+        game = 34223;
+        gameFound = true;
+        gameName = 'Valorant'
+      } else { sendMessage(message, `I could not find the specified game. Do \`t!help\` to get command info.`); }
 
-          if (gameFound) {
-            sendMessage(message, `Searching for the top 10 upcoming ${gameName} tournaments...`);
-            var perPage = 10;
-            var videogameId = game;
-            var query = `query TournamentsByVideogame($videogameId: ID!) {
+      if (gameFound) {
+        sendMessage(message, `Searching for the top 10 upcoming ${gameName} tournaments...`);
+        var perPage = 10;
+        var videogameId = game;
+        var query = `query TournamentsByVideogame($videogameId: ID!) {
                             tournaments(query: {
                               perPage: 10
                               page: 1
@@ -56,78 +57,77 @@ module.exports = {
                             }
                           }`;
 
-            fetch('https://api.smash.gg/gql/alpha', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': 'Bearer ' + SMASHGGTOKEN
-              },
-              body: JSON.stringify({
-                query,
-                variables: { perPage, videogameId },
-              })
-            })
-              .then(r => r.json())
-              .then(data => {
-                let upcomingTournaments = data.data.tournaments.nodes;
-                let tournamentArray = [[], [], [], [], [], [], [], [], [], []];
-                for (i = 0; i < upcomingTournaments.length; i++) {
-                  tournamentArray[i].push(upcomingTournaments[i]);
-                }
-                const generateEmbed = index => {
-                  let iconurl;
-                  let backgroundurl;
-                  // replace below with ternary operator
-                  if (tournamentArray[index][0].images[0]) {
-                    iconurl = tournamentArray[index][0].images[0].url;
+        fetch('https://api.smash.gg/gql/alpha', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + SMASHGGTOKEN
+          },
+          body: JSON.stringify({
+            query,
+            variables: { perPage, videogameId },
+          })
+        })
+          .then(r => r.json())
+          .then(data => {
+            let upcomingTournaments = data.data.tournaments.nodes;
+            let tournamentArray = [[], [], [], [], [], [], [], [], [], []];
+            for (i = 0; i < upcomingTournaments.length; i++) {
+              tournamentArray[i].push(upcomingTournaments[i]);
+            }
+            const generateEmbed = index => {
+              let iconurl;
+              let backgroundurl;
+              // replace below with ternary operator
+              if (tournamentArray[index][0].images[0]) {
+                iconurl = tournamentArray[index][0].images[0].url;
+              }
+              if (tournamentArray[index][0].images[1]) {
+                backgroundurl = tournamentArray[index][0].images[1].url;
+              }
+              const results = new Discord.MessageEmbed()
+                .setColor('#222326')
+                .setTitle(tournamentArray[index][0].name)
+                .setURL(`https://smash.gg/${tournamentArray[index][0].slug}`)
+                .setThumbnail(iconurl)
+                .setImage(backgroundurl)
+                .setFooter(`Tournament ${index + 1} of 10`, 'https://cdn.discordapp.com/attachments/719461475848028201/777094320531439636/image.png');
+              return results;
+            }
+
+            let currentIndex = 0;
+            message.channel.send(generateEmbed(currentIndex)).then(message => {
+              message.react('⬅️');
+              message.react('➡️');
+              const filter = (reaction, user) => {
+                return (reaction.emoji.name === '⬅️' || reaction.emoji.name === '➡️') && user.id != message.author.id;
+              };
+
+              const collector = message.createReactionCollector(filter, { time: 300000 });
+
+              collector.on('collect', (reaction, user) => {
+
+                if (reaction.emoji.name === '➡️') {
+                  if (currentIndex < 9) {
+                    currentIndex++;
+                    message.edit(generateEmbed(currentIndex));
                   }
-                  if (tournamentArray[index][0].images[1]) {
-                    backgroundurl = tournamentArray[index][0].images[1].url;
+                } else {
+                  if (currentIndex > 0) {
+                    currentIndex--;
+                    message.edit(generateEmbed(currentIndex));
                   }
-                  const results = new Discord.MessageEmbed()
-                    .setColor('#222326')
-                    .setTitle(tournamentArray[index][0].name)
-                    .setURL(`https://smash.gg/${tournamentArray[index][0].slug}`)
-                    .setThumbnail(iconurl)
-                    .setImage(backgroundurl)
-                    .setFooter(`Tournament ${index + 1} of 10`, 'https://cdn.discordapp.com/attachments/719461475848028201/777094320531439636/image.png');
-                  return results;
                 }
+              });
 
-                let currentIndex = 0;
-                message.channel.send(generateEmbed(currentIndex)).then(message => {
-                  message.react('⬅️');
-                  message.react('➡️');
-                  const filter = (reaction, user) => {
-                    return (reaction.emoji.name === '⬅️' || reaction.emoji.name === '➡️') && user.id != message.author.id;
-                  };
+              collector.on('end', collected => {
+                console.log(`Done collecting for search`);
+              });
+            }).catch(err => console.log(err));
 
-                  const collector = message.createReactionCollector(filter, { time: 300000 });
-
-                  collector.on('collect', (reaction, user) => {
-
-                    if (reaction.emoji.name === '➡️') {
-                      if (currentIndex < 9) {
-                        currentIndex++;
-                        message.edit(generateEmbed(currentIndex));
-                      }
-                    } else {
-                      if (currentIndex > 0) {
-                        currentIndex--;
-                        message.edit(generateEmbed(currentIndex));
-                      }
-                    }
-                  });
-
-                  collector.on('end', collected => {
-                    console.log(`Done collecting for search`);
-                  });
-                }).catch(err => console.log(err));
-
-              }).catch(err => console.log(err));
-          } else { sendMessage(message, `I could not find the specified game. Do \`t!help\` to get command info.`); }
-        } else { sendMessage(message, `There is no game provided. Do \`t!help\` to get command info.`); }
-      } else { sendMessage(message, 'you don\'t have the permissions for that :sob:', 'REPLY'); }
+          }).catch(err => console.log(err));
+      } else { sendMessage(message, `I could not find the specified game. Do \`t!help\` to get command info.`); }
+    } else { sendMessage(message, `There is no game provided. Do \`t!help\` to get command info.`); }
   },
 };
