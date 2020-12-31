793ab54710fb6b6f10f180a2babf29dd2651b522
Ayaan-Govil <38332695+Ayaan-Govil@users.noreply.github.com>
2020-12-31 04:37:28 -0800

    Help command update
    Changed help command to use message collector alongside emoji collector for ease of access (emojis were reacting too slowly and were susceptible to bugs).

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const { generateHelpSelection, generateAccountsEmbed, generateResultsEmbed, generateDQPingingEmbed, generateAnnounceEmbed, generateLocalizationEmbed, generateMatchmakingEmbed, generateSearchEmbed, generateInfoEmbed } = require('./help_embeds/help_embeds.js');
 const { generateHelp } = require('./help_embeds/generate_help.js');
 const { sendMessage } = require('../functions.js');
+const { closest } = require('fastest-levenshtein');
 
 module.exports = {
   name: 'help',
@@ -10,7 +11,7 @@ module.exports = {
   execute(message, client) {
     if (message.channel instanceof Discord.DMChannel) {
       console.log(`${message.author.tag} executed t!help in DM`);
-      sendMessage(message, `Please run the help command in a server. Due to permissions, the proper help command does not work in DMs.
+      sendMessage(message, `Please run the help command in a server. Due to permissions, the help command does not work in DMs.
       
 Feel free to contact **F0ne#1933** with any specific issues or feedback, or join the [support/development server](https://discord.gg/ssYPUk6Snc).`);
       return;
@@ -18,85 +19,145 @@ Feel free to contact **F0ne#1933** with any specific issues or feedback, or join
 
     if (message.author.tag != 'TournaBot#7477') console.log(`${message.author.tag} executed t!help`);
 
-    var currentIndex = 0;
+    const userMessage = message;
+    let currentIndex = 0;
     message.channel.send(generateHelpSelection(currentIndex)).then(message => {
-      let emojiArray = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '◀️', '▶️'];
-      let firstEmojiArray = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '◀️', '▶️'];
-      let secondEmojiArray = ['5️⃣', '6️⃣', '7️⃣', '8️⃣', '◀️', '▶️'];
-      for (i = 0; i < firstEmojiArray.length; i++) {
-        message.react(firstEmojiArray[i]);
-      }
 
-      const filter = (reaction, user) => {
-        return (emojiArray.includes(reaction.emoji.name)) && user.id != message.author.id;
-      };
+      // MESSAGE COLLECTOR
 
-      const collector = message.createReactionCollector(filter, { time: 180000 });
+      const filter = m => m.author.id === userMessage.author.id && m.author.id != message.author.id && !m.author.bot;
 
-      collector.on('collect', (reaction, user) => {
-        switch (reaction.emoji.name) {
+      const collector = new Discord.MessageCollector(message.channel, filter, { time: 60000, max: 2, maxMatches: 2 });
+      let matches = 0;
+
+      collector.on('collect', m => {
+        matches++;
+        switch (m.content.toLowerCase()) {
           // Accounts
-          case '1️⃣':
+          case 'accounts':
             generateHelp(message, client, generateAccountsEmbed, 3);
             break;
 
           // Results
-          case '2️⃣':
+          case 'user tournament results':
+          case 'results':
             generateHelp(message, client, generateResultsEmbed, 1);
             break;
 
           // DQ Pinging
-          case '3️⃣':
+          case 'dq pinging (automatic match calling)':
+          case 'dq pinging':
             generateHelp(message, client, generateDQPingingEmbed, 3);
             break;
           // Announcing
-          case '4️⃣':
+          case 'tournament announcing':
+          case 'announcing':
             generateHelp(message, client, generateAnnounceEmbed, 4);
             break;
 
           // Localization
-          case '5️⃣':
+          case 'localization':
             generateHelp(message, client, generateLocalizationEmbed, 2);
             break;
 
           // Matchmaking
-          case '6️⃣':
+          case 'matchmaking':
             generateHelp(message, client, generateMatchmakingEmbed, 5);
             break;
 
           // Search
-          case '7️⃣':
+          case 'tournament searching':
+          case 'searching':
             generateHelp(message, client, generateSearchEmbed, 1);
             break;
 
           // More Info
-          case '8️⃣':
+          case 'more info':
             generateHelp(message, client, generateInfoEmbed, 1);
             break;
+
+          default:
+            if (matches < 2) userMessage.reply(`There is no category called \`${m.content}\`. Did you mean \`${closest(m.content, ['accounts', 'user tournament results', 'results', 'dq pinging (automatic match calling)', 'dq pinging', 'tournament announcing', 'announcing', 'localization', 'matchmaking', 'tournament searching', 'searching', 'more info'])}\`?`);
+        }
+      });
+
+      // EMOJI COLLECTOR
+
+      let emojiArray = ['◀️', '▶️'];
+      for (i = 0; i < emojiArray.length; i++) {
+        message.react(emojiArray[i]).catch(err => console.log(err));
+      }
+
+      // let emojiArray = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '◀️', '▶️'];
+      // let firstEmojiArray = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '◀️', '▶️'];
+      // let secondEmojiArray = ['5️⃣', '6️⃣', '7️⃣', '8️⃣', '◀️', '▶️'];
+      // for (i = 0; i < firstEmojiArray.length; i++) {
+      //   message.react(firstEmojiArray[i]).catch(err => console.log(err));
+      // }
+
+      const emojiFilter = (reaction, user) => {
+        return user.id != message.author.id && !user.bot;
+      };
+
+      const emojiCollector = message.createReactionCollector(emojiFilter, { time: 60000 });
+
+      emojiCollector.on('collect', (reaction, user) => {
+        switch (reaction.emoji.name) {
+          //     // Accounts
+          //     case '1️⃣':
+          //       generateHelp(message, client, generateAccountsEmbed, 3);
+          //       break;
+
+          //     // Results
+          //     case '2️⃣':
+          //       generateHelp(message, client, generateResultsEmbed, 1);
+          //       break;
+
+          //     // DQ Pinging
+          //     case '3️⃣':
+          //       generateHelp(message, client, generateDQPingingEmbed, 3);
+          //       break;
+          //     // Announcing
+          //     case '4️⃣':
+          //       generateHelp(message, client, generateAnnounceEmbed, 4);
+          //       break;
+
+          //     // Localization
+          //     case '5️⃣':
+          //       generateHelp(message, client, generateLocalizationEmbed, 2);
+          //       break;
+
+          //     // Matchmaking
+          //     case '6️⃣':
+          //       generateHelp(message, client, generateMatchmakingEmbed, 5);
+          //       break;
+
+          //     // Search
+          //     case '7️⃣':
+          //       generateHelp(message, client, generateSearchEmbed, 1);
+          //       break;
+
+          //     // More Info
+          //     case '8️⃣':
+          //       generateHelp(message, client, generateInfoEmbed, 1);
+          //       break;
 
           case '▶️':
             if (currentIndex < 1) {
               currentIndex++;
-              message.reactions.removeAll();
-              message.edit(generateHelpSelection(currentIndex));
-              for (i = 0; i < secondEmojiArray.length; i++) {
-                message.react(secondEmojiArray[i]);
-              }
+              message.edit(generateHelpSelection(currentIndex)).catch(err => console.log(err));
             }
             break;
 
           case '◀️':
             if (currentIndex > 0) {
               currentIndex--;
-              message.reactions.removeAll();
-              message.edit(generateHelpSelection(currentIndex));
-              for (i = 0; i < firstEmojiArray.length; i++) {
-                message.react(firstEmojiArray[i]);
-              }
+              message.edit(generateHelpSelection(currentIndex)).catch(err => console.log(err));
             }
             break;
         }
       });
+
     }).catch(err => console.log(err));
   },
 };
