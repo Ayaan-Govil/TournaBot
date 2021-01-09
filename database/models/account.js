86e16151f317b4ff2605cb5469e76f32b3cd384b
Ayaan-Govil <38332695+Ayaan-Govil@users.noreply.github.com>
2021-01-09 09:29:40 -0800

    v4.0.0 Update - Automatic tournament reminders!

--- database/models/account.js
+++ database/models/account.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const accountSchema = new mongoose.Schema({
     discordtag: { type: mongoose.Schema.Types.Mixed },
     discordid: { type: mongoose.Schema.Types.Mixed },
-    profileslug: { type: mongoose.Schema.Types.Mixed }
+    profileslug: { type: mongoose.Schema.Types.Mixed },
+    reminder: { type: mongoose.Schema.Types.Mixed }
 });
 
 const accountModel = module.exports = mongoose.model('userids', accountSchema);
