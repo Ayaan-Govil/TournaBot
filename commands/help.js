794885d266ba92d5f399f43d199a560867355e0c
Ayaan-Govil <38332695+Ayaan-Govil@users.noreply.github.com>
2021-01-09 09:20:30 -0800

    v4.0.0 Update - Automatic tournament reminders!
    This update introduces an automatic tournament reminder system, which messages users an hour before their tournaments begin. There are also several other fixes/changes scattered throughout the code.

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 // Dependencies
 const Discord = require('discord.js');
-const { generateHelpSelection, generateAccountsEmbed, generateResultsEmbed, generateDQPingingEmbed, generateAnnounceEmbed, generateLocalizationEmbed, generateMatchmakingEmbed, generateSearchEmbed, generateInfoEmbed } = require('./help_embeds/help_embeds.js');
+const { generateHelpSelection, generateAccountsEmbed, generateReminderEmbed, generateResultsEmbed, generateDQPingingEmbed, generateAnnounceEmbed, generateLocalizationEmbed, generateMatchmakingEmbed, generateSearchEmbed, generateInfoEmbed } = require('./help_embeds/help_embeds.js');
 const { generateHelp } = require('./help_embeds/generate_help.js');
 const { sendMessage } = require('../functions.js');
 const { closest } = require('fastest-levenshtein');
@@ -8,7 +8,8 @@ const { closest } = require('fastest-levenshtein');
 module.exports = {
   name: 'help',
   description: 'Help command.',
-  execute(message, client) {
+  execute(message, client, userMessage) {
+    // replace top "reply with" message with message.reply
     if (message.channel instanceof Discord.DMChannel) {
       console.log(`${message.author.tag} executed t!help in DM`);
       sendMessage(message, `Please run the help command in a server. Due to permissions, the help command does not work in DMs.
@@ -19,15 +20,16 @@ Feel free to contact **F0ne#1933** with any specific issues or feedback, or join
 
     if (message.author.tag != 'TournaBot#7477') console.log(`${message.author.tag} executed t!help`);
 
-    const userMessage = message;
+    //const userMessage = message;
     let currentIndex = 0;
     message.channel.send(generateHelpSelection(currentIndex)).then(message => {
-
+      userMessage.reply('please reply with the category you would like to see:').catch(err => console.log(err));
+      
       // MESSAGE COLLECTOR
 
       const filter = m => m.author.id === userMessage.author.id && m.author.id != message.author.id && !m.author.bot;
 
-      const collector = new Discord.MessageCollector(message.channel, filter, { time: 60000, max: 2, maxMatches: 2 });
+      const collector = new Discord.MessageCollector(message.channel, filter, { time: 120000, max: 2, maxMatches: 2 });
       let matches = 0;
 
       collector.on('collect', m => {
@@ -35,49 +37,55 @@ Feel free to contact **F0ne#1933** with any specific issues or feedback, or join
         switch (m.content.toLowerCase()) {
           // Accounts
           case 'accounts':
-            generateHelp(message, client, generateAccountsEmbed, 3);
+            generateHelp(message, client, generateAccountsEmbed, 3, m);
+            break;
+
+          case 'tournament reminders (beta)':
+          case 'tournament reminders':
+          case 'reminders':
+            generateHelp(message, client, generateReminderEmbed, 2, m);
             break;
 
           // Results
           case 'user tournament results':
           case 'results':
-            generateHelp(message, client, generateResultsEmbed, 1);
+            generateHelp(message, client, generateResultsEmbed, 1, m);
             break;
 
           // DQ Pinging
           case 'dq pinging (automatic match calling)':
           case 'dq pinging':
-            generateHelp(message, client, generateDQPingingEmbed, 3);
+            generateHelp(message, client, generateDQPingingEmbed, 3, m);
             break;
           // Announcing
           case 'tournament announcing':
           case 'announcing':
-            generateHelp(message, client, generateAnnounceEmbed, 4);
+            generateHelp(message, client, generateAnnounceEmbed, 4, m);
             break;
 
           // Localization
           case 'localization':
-            generateHelp(message, client, generateLocalizationEmbed, 2);
+            generateHelp(message, client, generateLocalizationEmbed, 2, m);
             break;
 
           // Matchmaking
           case 'matchmaking':
-            generateHelp(message, client, generateMatchmakingEmbed, 5);
+            generateHelp(message, client, generateMatchmakingEmbed, 5, m);
             break;
 
           // Search
           case 'tournament searching':
           case 'searching':
-            generateHelp(message, client, generateSearchEmbed, 1);
+            generateHelp(message, client, generateSearchEmbed, 1, m);
             break;
 
           // More Info
           case 'more info':
-            generateHelp(message, client, generateInfoEmbed, 1);
+            generateHelp(message, client, generateInfoEmbed, 1, m);
             break;
 
           default:
-            if (matches < 2) userMessage.reply(`There is no category called \`${m.content}\`. Did you mean \`${closest(m.content, ['accounts', 'user tournament results', 'results', 'dq pinging (automatic match calling)', 'dq pinging', 'tournament announcing', 'announcing', 'localization', 'matchmaking', 'tournament searching', 'searching', 'more info'])}\`?`);
+            if (matches < 2) userMessage.reply(`There is no category called \`${m.content}\`. Did you mean \`${closest(m.content, ['accounts', 'tournament reminders (beta)', 'tournament reminders', 'reminders', 'user tournament results', 'results', 'dq pinging (automatic match calling)', 'dq pinging', 'tournament announcing', 'announcing', 'localization', 'matchmaking', 'tournament searching', 'searching', 'more info'])}\`?`).catch(err => console.log(err));
         }
       });
 
@@ -88,60 +96,14 @@ Feel free to contact **F0ne#1933** with any specific issues or feedback, or join
         message.react(emojiArray[i]).catch(err => console.log(err));
       }
 
-      // let emojiArray = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '◀️', '▶️'];
-      // let firstEmojiArray = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '◀️', '▶️'];
-      // let secondEmojiArray = ['5️⃣', '6️⃣', '7️⃣', '8️⃣', '◀️', '▶️'];
-      // for (i = 0; i < firstEmojiArray.length; i++) {
-      //   message.react(firstEmojiArray[i]).catch(err => console.log(err));
-      // }
-
       const emojiFilter = (reaction, user) => {
         return user.id != message.author.id && !user.bot;
       };
 
-      const emojiCollector = message.createReactionCollector(emojiFilter, { time: 60000 });
+      const emojiCollector = message.createReactionCollector(emojiFilter, { time: 120000 });
 
       emojiCollector.on('collect', (reaction, user) => {
         switch (reaction.emoji.name) {
-          //     // Accounts
-          //     case '1️⃣':
-          //       generateHelp(message, client, generateAccountsEmbed, 3);
-          //       break;
-
-          //     // Results
-          //     case '2️⃣':
-          //       generateHelp(message, client, generateResultsEmbed, 1);
-          //       break;
-
-          //     // DQ Pinging
-          //     case '3️⃣':
-          //       generateHelp(message, client, generateDQPingingEmbed, 3);
-          //       break;
-          //     // Announcing
-          //     case '4️⃣':
-          //       generateHelp(message, client, generateAnnounceEmbed, 4);
-          //       break;
-
-          //     // Localization
-          //     case '5️⃣':
-          //       generateHelp(message, client, generateLocalizationEmbed, 2);
-          //       break;
-
-          //     // Matchmaking
-          //     case '6️⃣':
-          //       generateHelp(message, client, generateMatchmakingEmbed, 5);
-          //       break;
-
-          //     // Search
-          //     case '7️⃣':
-          //       generateHelp(message, client, generateSearchEmbed, 1);
-          //       break;
-
-          //     // More Info
-          //     case '8️⃣':
-          //       generateHelp(message, client, generateInfoEmbed, 1);
-          //       break;
-
           case '▶️':
             if (currentIndex < 1) {
               currentIndex++;
