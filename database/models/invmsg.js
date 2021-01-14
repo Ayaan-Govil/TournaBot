c457ce24cacd64cd01cf5ed480895c542a876839
Ayaan-Govil <38332695+Ayaan-Govil@users.noreply.github.com>
2021-01-14 14:17:00 -0800

    Delete invmsg model

--- database/models/invmsg.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const invmsgSchema = new mongoose.Schema({
-    guildid: { type: mongoose.Schema.Types.Mixed }
-});
-
-const invmsgModel = module.exports = mongoose.model('invmsgtoggles', invmsgSchema);
