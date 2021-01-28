aed604963318a417de63f02f88c61a1ab0e0fd30
Ayaan-Govil <38332695+Ayaan-Govil@users.noreply.github.com>
2021-01-28 15:17:23 -0800

    v4.1.4 Update - Rewritten t!search
    This update fixes several issues with t!search and adds more info from tournament reminders into the embed.

--- commands/search.js
+++ commands/search.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 const { SMASHGGTOKEN } = require('../config.json');
 const fetch = require('node-fetch');
-const { sendMessage } = require('../functions');
+const { convertEpoch, convertEpochToClock, sendMessage } = require('../functions');
 
 module.exports = {
   name: 'search',
@@ -21,43 +21,70 @@ module.exports = {
     let gameFound = false;
     let gameName;
     if (gameArgs.length >= 1) {
-      if (gameArgs.join(' ').toLowerCase() == 'super smash bros. ultimate') {
-        game = 1386;
-        gameFound = true;
-        gameName = 'Super Smash Bros. Ultimate';
-      } else if (gameArgs.join(' ').toLowerCase() == 'valorant') {
-        game = 34223;
-        gameFound = true;
-        gameName = 'Valorant'
-      } else { sendMessage(message, `I could not find the specified game. Do \`t!help\` to get command info.`); }
+
+      switch (gameArgs.join(' ').toLowerCase()) {
+        case 'super smash bros. ultimate':
+        case 'super smash bros ultimate':
+        case 'smash bros. ultimate':
+        case 'smash bros ultimate':
+        case 'smash ultimate':
+        case 'ultimate':
+          game = 1386;
+          gameFound = true;
+          gameName = 'Super Smash Bros. Ultimate';
+          break;
+
+        case 'valorant':
+          game = 34223;
+          gameFound = true;
+          gameName = 'Valorant'
+          break;
+      }
 
       if (gameFound) {
-        sendMessage(message, `Searching for the top 10 upcoming ${gameName} tournaments...`);
+        sendMessage(message, `Searching for upcoming ${gameName} tournaments...`);
         var perPage = 10;
         var videogameId = game;
         var query = `query TournamentsByVideogame($videogameId: ID!) {
-                            tournaments(query: {
-                              perPage: 10
-                              page: 1
-                              sortBy: "startAt asc"
-                              filter: {
-                              upcoming: true
-                              videogameIds: [
-                              $videogameId
-                                ]
-                              }
-                            }) {
-                              nodes {
-                                name
-                                slug
-                                images {
-                                  height
-                                  width
-                                  url
-                                }
-                              }
+                        tournaments(query: {
+                          perPage: 10
+                          page: 1
+                          sortBy: "startAt asc"
+                          filter: {
+                          upcoming: true
+                          videogameIds: [
+                          $videogameId
+                            ]
+                          }
+                        }) {
+                          nodes {
+                            id
+                            name
+                            slug
+                            numAttendees
+                            startAt
+                            isOnline
+                            images {
+                              height
+                              width
+                              url
                             }
-                          }`;
+                            events {
+                              id
+                              name
+                              numEntrants
+                              startAt
+                              checkInEnabled
+                              checkInBuffer
+                              checkInDuration
+                            }
+                            streams {
+                              streamSource
+                              streamName
+                            }
+                          }
+                        }
+                      }`;
 
         fetch('https://api.smash.gg/gql/alpha', {
           method: 'POST',
@@ -74,26 +101,68 @@ module.exports = {
           .then(r => r.json())
           .then(data => {
             let upcomingTournaments = data.data.tournaments.nodes;
-            let tournamentArray = [[], [], [], [], [], [], [], [], [], []];
+            let tournamentArray = [];
+
             for (i = 0; i < upcomingTournaments.length; i++) {
-              tournamentArray[i].push(upcomingTournaments[i]);
+              tournamentArray[i] = upcomingTournaments[i];
             }
-            //if (tournamentArray[index][0]
+
             const generateEmbed = index => {
-              let imageurl = ['', ''];
-              for (let image of tournamentArray[index][0].images) {
-                if (image) {
-                  image.height === image.width ? imageurl[0] = image.url : imageurl[1] = image.url;
+
+              return searchEmbed();
+
+              function searchEmbed() {
+                let tournament = tournamentArray[index];
+
+                let imageurl = ['', ''];
+                for (let image of tournament.images) {
+                  if (image) image.height === image.width ? imageurl[0] = image.url : imageurl[1] = image.url;
                 }
+
+                let tournamentOnline = 'Offline';
+                if (tournament.isOnline) tournamentOnline = 'Online';
+
+                let events = [];
+                for (e = 0; e < tournament.events.length; e++) {
+                  if (e < 3) {
+                    events.push(`\`${tournament.events[e].name}\``);
+                    events.push(`${tournament.events[e].numEntrants} Entrants`);
+                    events.push(`*${convertEpoch(tournament.events[e].startAt, 'America/Los_Angeles')}*`);
+                    if (tournament.events[e].checkInEnabled) events.push(`__Check-in opens at ${convertEpochToClock(tournament.events[e].startAt - tournament.events[e].checkInBuffer - tournament.events[e].checkInDuration, 'America/Los_Angeles', false)} and closes at ${convertEpochToClock(tournament.events[e].startAt - tournament.events[e].checkInBuffer, 'America/Los_Angeles', false)}.__`);
+                  } else {
+                    events.push('\`And more...\`');
+                    e = tournament.events.length;
+                  }
+                }
+
+                let streams = [];
+                if (tournament.streams) {
+                  for (let stream of tournament.streams) {
+                    if (stream.streamSource === 'TWITCH') {
+                      streams.push(`https://twitch.tv/${stream.streamName}`);
+                    }
+                  }
+                }
+
+                const searchEmbed = new Discord.MessageEmbed()
+                  .setColor('#222326')
+                  .setTitle(tournament.name)
+                  .setURL(`https://smash.gg/${tournament.slug}`)
+                  .setThumbnail(imageurl[0])
+                  .setImage(imageurl[1])
+                  .addFields(
+                    {
+                      name: 'Tournament Info', value: `
+${tournament.numAttendees} Attendees
+*${tournamentOnline} Tournament*
+*${convertEpoch(tournament.startAt, 'America/Los_Angeles')}*`, inline: true
+                    },
+                    { name: 'Events', value: events.join('\n'), inline: true },
+                  )
+                  .setFooter(`Tournament ${index + 1} of ${tournamentArray.length}`, 'https://cdn.discordapp.com/attachments/719461475848028201/777094320531439636/image.png');
+                if (streams.length) searchEmbed.addField('Streams', streams.join('\n'));
+                return searchEmbed;
               }
-              const results = new Discord.MessageEmbed()
-                .setColor('#222326')
-                .setTitle(tournamentArray[index][0].name)
-                .setURL(`https://smash.gg/${tournamentArray[index][0].slug}`)
-                .setThumbnail(imageurl[0])
-                .setImage(imageurl[1])
-                .setFooter(`Tournament ${index + 1} of 10`, 'https://cdn.discordapp.com/attachments/719461475848028201/777094320531439636/image.png');
-              return results;
             }
 
             let currentIndex = 0;
@@ -109,7 +178,7 @@ module.exports = {
               collector.on('collect', (reaction, user) => {
 
                 if (reaction.emoji.name === '➡️') {
-                  if (currentIndex < 9) {
+                  if (currentIndex < tournamentArray.length - 1) {
                     currentIndex++;
                     message.edit(generateEmbed(currentIndex));
                   }
